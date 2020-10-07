c1b4bade6870d219308ff29e73aa2f2e1712ac90
hrinal <hrinal.bunkar@zf.com>
2020-10-07 17:31:49 +0530

    Updates

---
+++ precache-manifest.8dd93e0821d3e3b0630cb5e69d692b40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e5724453721e0670121c935ea2eeacf",
+    "url": "/wishlist/index.html"
+  },
+  {
+    "revision": "2d0119385e6f7083d5b2",
+    "url": "/wishlist/static/css/main.6136ca74.chunk.css"
+  },
+  {
+    "revision": "b2871b493940f54c655a",
+    "url": "/wishlist/static/js/2.4a244b82.chunk.js"
+  },
+  {
+    "revision": "a9918bfdd2d292b2528f287dc5d4129a",
+    "url": "/wishlist/static/js/2.4a244b82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d0119385e6f7083d5b2",
+    "url": "/wishlist/static/js/main.b56db540.chunk.js"
+  },
+  {
+    "revision": "ce9b0b9b0ba44afce573",
+    "url": "/wishlist/static/js/runtime-main.06f01e0a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wishlist/static/media/logo.5d5d9eef.svg"
+  }
+]);
